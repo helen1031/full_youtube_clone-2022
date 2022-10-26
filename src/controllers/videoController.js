42cbff1d4f89904ae1de3376905f7d77289b87dc
helen1031 <32950301+helen1031@users.noreply.github.com>
2022-10-27 07:49:48 +0900

    Array DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,39 @@
+let videos = [
+  {
+    title: "Video #1",
+    rating : 5,
+    comments: 2,
+    createdAt : "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Video #2",
+    rating : 5,
+    comments: 2,
+    createdAt : "2 minutes ago",
+    views: 1,
+    id: 2,
+  },
+  {
+    title: "Video #3",
+    rating : 5, 
+    comments: 2,
+    createdAt : "2 minutes ago",
+    views: 59,
+    id: 3,
+  }
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Video #1",
-      rating : 5,
-      comments: 2,
-      createdAt : "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #2",
-      rating : 5,
-      comments: 2,
-      createdAt : "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Video #3",
-      rating : 5,
-      comments: 2,
-      createdAt : "2 minutes ago",
-      views: 59,
-      id: 1,
-    }
-  ];
   res.render("home", { pageTitle: "Home", videos});
 };
 export const search = (req, res)=> res.send("Search Video");
-export const see = (req, res) => res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id-1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video});
+};
 export const edit = (req, res) => {
   console.log(req.params);
   res.send("Edit Video");
